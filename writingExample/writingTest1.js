babaf37eb0ab191f423aa8418f225fa85082d2ac
jackSN8 <jackin0708@highgateschool.org.uk>
2021-03-23 22:25:13 +0000

    adding evolution code

---
+++ writingExample/writingTest1.js
@@ -0,0 +1,30 @@
+let length = 5;
+
+
+function setup()
+{
+  createCanvas(800,500);
+}
+
+function draw()
+{
+  let text_eg = '';
+  background(223);
+  noFill();
+  stroke(0);
+  textSize(32);
+  //text("Raptor looks healthy",150,250);
+  for(let i=0; i<length; i++)
+  {
+    text_eg += makeID(int(random()*62));
+  }
+  text(text_eg,150,250);
+}
+
+
+function makeID(inpNum)
+{
+  let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let output = characters.charAt(inpNum);
+  return output;
+}
