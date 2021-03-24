ed2572c025e05536ea586edccafafcd384cb6561
jackSN8 <jackin0708@highgateschool.org.uk>
2021-03-24 19:56:34 +0000

    Set up DNA file and fitness functions. Next is breeding algorithim.

--- writingExample/DNA.js
+++ writingExample/DNA.js
@@ -1,22 +1,41 @@
 class DNA
 {
-  this.genes = [];
-  constructor()
+  constructor(length)
   {
+    this.genes = [];
+    this.fitness;
+    //Creates the words with totally random characters
+    for(this.i=0; this.i<length; this.i++)
+    {
+      this.genes[this.i]=makeID(int(random()*27));//27 means only lower case
+      //53 means all letters, capital and non capital + space. 63 adds digits 0-9
+    }
   }
 
+  update()
+  {
+    this.fitness = fitness(target);
+  }
+
+  ///Function to calculate the fitness of a single word (words are
+  ///called DNAs which is a bit wierd.) Checks how many characters
+  ///are the same as a target. Obviousely in the real world fitness
+  ///is essentially a function of how much you fuck, whereas in this
+  ///case this determines how much the DNA gets to fuck
   fitness(target)
   {
     this.fitness=0;
     for(this.i=0; this.i<genes.length; this.i++)
     {
-      if(target[i])==this.genes[i]
+      if(target[this.i]==this.genes[this.i])
       {
         this.fitness++;
       }
     }
     return this.fitness;
   }
+
+
 
 
 }
--- writingExample/writingTest1.js
+++ writingExample/writingTest1.js
@@ -1,30 +1,57 @@
-let length = 5;
+let population = [];
+let popSize = 100;
+let target = 'raptor looks healthy';
 
 
 function setup()
 {
   createCanvas(800,500);
+  for(let i=0; i<popSize; i++)
+  {
+    population.push(new DNA(20));
+  }
 }
 
 function draw()
 {
-  let text_eg = '';
   background(27);
   noFill();
   stroke(0);
   textSize(32);
-  //text("Raptor looks healthy",150,250);
-  for(let i=0; i<length; i++)
+  ///Calculate the fitness of everything in the population
+  let popFitness = [];
+  for(let i=0; i<population.length; i++)
   {
-    text_eg += makeID(int(random()*62));
+    popFitness[i] = population[i].fitness(target);
   }
-  text(text_eg,150,250);
+  ///Then Normalize the fitness so that the entire populations fitness fits
+  let totalNum = sumArray(popFitness);
+  //Normalize by dividing the current val by total
+  for(let i=0; i<population.length; i++)
+  {
+    popFitness[i] /= totalNum;
+  }
+  ///Then make them do the sex
+
 }
 
 
+///Function that sums an array
+function sumArray(input)
+{
+  let total =0;
+  for(let i=0; i<input.lenth; i++)
+  {
+    total += input[i];
+  }
+  return total;
+}
+
+///Function that takes in a number from 0-63 and returns a char
+///at that point in the list below (all capitals, all non capitals, 0-9, space)
 function makeID(inpNum)
 {
-  let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let characters = ' abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
   let output = characters.charAt(inpNum);
   return output;
 }
