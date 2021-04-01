e747d8f8039b75cade082b4d2ffbadacff53daa4
jackSN8 <jackin0708@highgateschool.org.uk>
2021-04-01 08:09:58 +0100

    Added pathfinding algorithim, need to finish move function though.

--- blobTest1/agent.js
+++ blobTest1/agent.js
@@ -7,9 +7,9 @@ class agent
     this.size = tSize;
     this.color = tColor;
     this.position = tPosition;
-    this.velocity = createVector(1,0); //Normal speed of the agent
-    this.turnRate = 2; //Turnrate of the agent
-    this.accuracy = 4; //Heading accuracy of the agent
+    this.velocity = createVector(0,-1); //Normal speed of the agent
+    this.turnRate = PI/90; //Turnrate of the agent
+    this.accuracy = PI/45; //Heading accuracy of the agent
   }
 
   update()
@@ -24,13 +24,35 @@ class agent
     circle(this.position.x,this.position.y,this.size);
   }
 
+
+
+
   ///Function that takes in a target position, and rotates the agent towards target
   ///To move to it
   seek(target)
   {
     this.targetDif = p5.Vector.sub(this.position,target).mult(-1);
     line(this.position.x,this.position.y,this.position.x+this.targetDif.x,this.position.y+this.targetDif.y);
-    //this.headingDif = this.targetDif.heading()-this.heading;
+    // this.targetDif = this.targetDif.normalize(0);
+    // this.targetDif = p5.Vector.mult(this.targetDif,this.velocity.mag());
+    // this.velocityDif = this.targetDif.sub(this.velocity);
+    this.velHeading = vectorHeading(this.velocity);
+
+
+    this.headingToTarget = vectorHeading(this.targetDif);
+    text(this.velHeading,700,400);
+    text(this.headingToTarget,700,350);
+    this.headingDif = this.headingToTarget - this.velHeading;
+    if(this.headingDif < -PI)
+    {
+      this.headingDif = TWO_PI+this.headingDif;
+    }
+    text(this.headingDif,700,300);
+    if(this.headingDif > this.accuracy)
+    {
+      
+    }
+
 
   }
   ///Function to avoid the edges of the canvas
--- blobTest1/main.js
+++ blobTest1/main.js
@@ -5,13 +5,48 @@ function setup()
 {
   createCanvas(800,500);
   a = new agent(createVector(400,250),50,color(100,150,255));
-  b = createVector(500,100);
-  angleMode(DEGREES);
+  b = createVector(random(100,700),100);
+  angleMode(RADIANS);
 }
 
 function draw()
 {
   background(27);
+  textSize(8);
   a.update();
   circle(b.x,b.y,50);
+}
+
+
+function polarToCartesian(mag,heading)
+{
+  let x = cos(heading)*mag;
+  let y = sin(heading)*mag;
+  return createVector(x,y);
+}
+
+function cartesianToPolar(input)
+{
+  let heading = atan(input.y/input.x);
+  let mag = sqrt(input.x*input.y);
+  return createVector(mag,heading);
+}
+
+
+function vectorHeading(input)
+{
+  let heading = atan(input.y/input.x);
+  //If vector is in quadrant I heading is just arctan y
+  //If vector is in quadrant II or III, heading is arctan y + PI
+  if(input.x<0)
+  {
+    heading += PI;
+  }
+  //If vector is in quadrant IV, heading is 2PI + arctan y
+  if(input.x>=0 && input.y<0)
+  {
+    heading += TWO_PI;
+  }
+
+  return heading;
 }
