c6e9dc31ddb40b520bcb752259f5edbbf00e7f2a
Jack Kingdon <>
2021-04-22 13:53:24 +0000

    Added stuff

--- blobTest2/agent.js
+++ blobTest2/agent.js
@@ -1,15 +1,32 @@
 class agent
 {
-  constructor(position,velocity)
+  constructor(position,velocity,ms)
   {
     this.position = position;
     this.velocity = velocity;
+    this.acceleration = createVector(0,0);
+    this.mass = 1;
+    this.maxSpeed = 5;
   }
   update()
   {
-    this.position.add(this.velocity);
 
+
+
+
+
+    this.position.add(this.velocity);
+    this.velocity.add(this.acceleration);
+    this.acceleration.mult(0);
   }
+
+  seek(target)
+  {
+    let desired = this.target.sub(this.position);
+    desired.mult(this.maxSpeed/desired.mag);
+    let steering = desired.sub(this.velocity);
+  }
+
 
 
   applyForce(force)
